836b40b23852d28ec80e8254fcab5d5b06db2aa4
Henry Foster <henrythefoster@gmail.com>
2018-04-29 14:13:07 -0400

    Basic import of bio info

--- bio.js
+++ bio.js
@@ -4,4 +4,17 @@ function fillBio(){
 
 	document.getElementById("name").innerHTML = person.name;
 	document.getElementById("message").innerHTML = person.message;
+	document.getElementById("website").innerHTML = person.url;
+	
+	for(var i=0; i<person.project.length; i++){
+		document.getElementById("projects").innerHTML += person.project[i]+", ";
+	}
+	var proj = document.getElementById("projects").innerHTML;
+	proj = proj.substring(0, proj.length-3);
+
+	for(var i=0; i<person.terms_on.length; i++){
+		document.getElementById("terms").innerHTML += person.terms_on[i]+", ";
+	}
+	var proj = document.getElementById("terms").innerHTML;
+	proj = proj.substring(0, proj.length-3);
 }
