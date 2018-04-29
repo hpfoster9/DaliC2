824f034853756615dd1cb0d712b847144036dcfa
Henry Foster <henrythefoster@gmail.com>
2018-04-29 11:38:29 -0400

    implemented the map api with the points

--- myjs.js
+++ myjs.js
@@ -1,30 +1,54 @@
-var data = [{
-  type:'scattermapbox',
-  lat:['45.5017'],
-  lon:['-73.5673'],
-  mode:'markers',
-  marker: {
-    size:14
-  },
-  text:['Montreal']
-}]
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var obj = JSON.parse(this.responseText);
+      loadMap(obj);
+    }
+  };
+  xhttp.open("GET", "http://mappy.dali.dartmouth.edu/members.json", true);
+  xhttp.send();
+}
+loadDoc();
 
-var layout = {
-  autosize: true,
-  hovermode:'closest',
-  mapbox: {
-    bearing:0,
-    center: {
-      lat:45,
-      lon:-73
-    },
-    pitch:0,
-    zoom:3
-  },
+function Datapoint(name,lat,lon){
+  this.type = 'scattermapbox';
+  this.lat = [lat];
+  this.lon = [lon];
+  this.mode = 'markers';
+  this.marker = {size:14};
+  this.text = [name];
 }
 
-Plotly.setPlotConfig({
+function loadMap(jArray){
+  console.log(jArray);
+  var layout = {
+    autosize: true,
+    hovermode:'closest',
+    mapbox: {
+      bearing:0,
+      center: {
+        lat:45,
+        lon:-73
+      },
+      pitch:0,
+      zoom:3
+    },
+  }
+  var data = [];
+
+  jArray.forEach( function(p){
+    data.push(new Datapoint(p.name, p.lat_long[0], p.lat_long[1]));
+  });
+    
+
+  Plotly.setPlotConfig({
   mapboxAccessToken: 'pk.eyJ1IjoiZXRwaW5hcmQiLCJhIjoiY2luMHIzdHE0MGFxNXVubTRxczZ2YmUxaCJ9.hwWZful0U2CQxit4ItNsiQ'
-})
-var div = document.getElementById("myDiv");
-Plotly.plot(div, data, layout)
+  });
+
+  var div = document.getElementById("myDiv");
+  Plotly.plot(div, data, layout);
+}
+
+
+
